8ce0f4188f99a1476974c3956e10b4a7c488d2f2
BkAngel201 <33973377+BkAngel201@users.noreply.github.com>
2020-09-10 21:39:38 -0400

    Stretch finished

--- client/src/User.js
+++ client/src/User.js
@@ -1,12 +1,35 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import axios from 'axios';
 
 
 
 function User(props) {
-    let params = useParams()
+    let [userInfo, setUserInfo] = useState([])
+    let {user_id} = useParams()
+
+    useEffect(() => {
+        axios.get(`http://localhost:5000/api/users/${user_id}/posts`)
+            .then(res => {
+                setUserInfo(res.data)
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    },[])
     return (
-    <div>{params.id77777777}</div>
+        <>
+            <div>{userInfo[0] !== undefined ? userInfo[0].postedBy : null}</div>
+            <div>
+                <h3>Posts:</h3>
+                {
+                    userInfo.map(post => (
+                        <p>{post.text}</p>
+                    ))
+                }
+            </div>
+            <Link to="/">AtraS</Link>
+        </>
     )
 }
 
